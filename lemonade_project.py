20f449ad3421166910a89bda56228b0d9bf18c05
moosesmoose13 <127569368+moosesmoose13@users.noreply.github.com>
2023-04-02 16:53:14 -0600

    Add files via upload

---
+++ lemonade_project.py
@@ -0,0 +1,15 @@
+# Define the prices of the items
+lemon_cost, sugar_cost, cup_cost = 0.5, 0.2, 0.1
+
+# Calculate the total cost of the items
+total_cost = lemon_cost + sugar_cost + cup_cost
+
+# Calculate the remaining investment after purchasing the items
+remaining_investment = 50 - total_cost
+
+# Print out the prices of the items, the total cost, and the remaining investment
+print(f"Price of lemons: ${lemon_cost}")
+print(f"Price of sugar: ${sugar_cost}")
+print(f"Price of cups: ${cup_cost}")
+print(f"Total cost: ${total_cost}")
+print(f"Remaining investment: ${remaining_investment}")
